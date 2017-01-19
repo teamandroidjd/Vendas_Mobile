b8f87a6b70d636d09e1ce83415fb131f240b3d38
eduardo.costa <eduardo@jdsystem.com.br>
2017-01-19 16:15:32 -0200

    Primeira

---
+++ app/src/main/java/com/jdsystem/br/vendasmobile/Model/SqliteParametroDao.java
@@ -0,0 +1,84 @@
+package com.jdsystem.br.vendasmobile.Model;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteStatement;
+import android.util.Log;
+
+import com.jdsystem.br.vendasmobile.ConfigDB;
+
+public class SqliteParametroDao {
+
+
+    private Context ctx;
+    private String sql;
+    private boolean gravacao;
+
+
+    public SqliteParametroDao(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    public SqliteParametroBean busca_parametros() {
+
+        SQLiteDatabase db = new ConfigDB(ctx).getReadableDatabase();
+        SqliteParametroBean parametro = null;
+
+        sql = "select * from PARAMAPP";
+
+        try {
+
+            Cursor cursor = db.rawQuery(sql, null);
+
+            if (cursor.moveToFirst()) {
+                try {
+
+                    parametro = new SqliteParametroBean();
+                    parametro.setP_usu_codigo(cursor.getInt(cursor.getColumnIndex(parametro.P_CODIGO_USUARIO)));
+                    parametro.setP_importar_todos_clientes(cursor.getString(cursor.getColumnIndex(parametro.P_IMPORTAR_TODOS_CLIENTES)));
+                    parametro.setP_end_ip_local(cursor.getString(cursor.getColumnIndex(parametro.P_ENDERECO_IP_LOCAL)));
+                    parametro.setP_end_ip_remoto(cursor.getString(cursor.getColumnIndex(parametro.P_ENDERECO_IP_REMOTO)));
+                    parametro.setP_trabalhar_com_estoque_negativo(cursor.getString(cursor.getColumnIndex(parametro.P_ESTOQUE_NEGATIVO)));
+                    //parametro.setP_desconto_do_vendedor(cursor.getString(cursor.getColumnIndex(parametro.P_DESCONTO_MAX())));
+                    parametro.setP_desconto_do_vendedor(cursor.getDouble(cursor.getColumnIndex(parametro.P_DESCONTO_VENDEDOR)));
+                    int Desc = cursor.getInt(cursor.getColumnIndex(parametro.P_DESCONTO_VENDEDOR));
+                    parametro.setP_usuario(cursor.getString(cursor.getColumnIndex(parametro.P_USUARIO)));
+                    parametro.setP_senha(cursor.getString(cursor.getColumnIndex(parametro.P_SENHA)));
+                    parametro.setP_qual_endereco_ip(cursor.getString(cursor.getColumnIndex(parametro.P_QUAL_END_IP)));
+                }catch (Exception E){
+                    E.toString();
+                }
+            }
+
+        } catch (SQLiteException e) {
+            Log.d("Script", e.getMessage());
+        }finally {
+            db.close();
+
+        }
+
+        return parametro;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/jdsystem/br/vendasmobile/adapter/DocsListAdapter.java
@@ -0,0 +1,29 @@
+package com.jdsystem.br.vendasmobile.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SimpleAdapter;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by eduardo.costa on 22/10/2016.
+ */
+
+public class DocsListAdapter extends SimpleAdapter {
+    private int[] colors = new int[] {0x30fefeff, 0x308c9cef};//  0x30ffffff, 0x30808080 };
+
+    public DocsListAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
+        super(context, data, resource, from, to);
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = super.getView(position, convertView, parent);
+
+        int colorPos = position % colors.length;
+        view.setBackgroundColor(colors[colorPos]);
+        return view;
+    }
+}
